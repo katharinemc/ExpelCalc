a076b3caf724e9b604a976e29096dc438c94ac10
Katharine McLeod <katharinemcdevelopment@gmail.com>
2018-06-13 08:33:17 -0400

    initial number buttons working

--- src/NumberButtons.js
+++ src/NumberButtons.js
@@ -1,10 +1,11 @@
 import React from 'react';
 export default function NumberButton(props) {
+
 let buttons=[]
-    for(let i = 0; i <= 9; i++){
-        buttons.push(<button className="numberbutton" type='button' key={i}>'{i}'</button>)
+    for(let i = 1; i <= 9; i++){
+        buttons.push(<button className="numberbutton"  type='button' id={i} onClick={() => props.onClick(buttons[i-1].props.id)}>'{i}'</button>)
     }
-let    zero = buttons.shift()
+// let    zero = buttons.shift()
 
     console.log( buttons)
 return (<span className=""> {buttons}</span>)
--- src/WholeCalc.js
+++ src/WholeCalc.js
@@ -6,35 +6,45 @@ import MathOperators from './MathOperators'
 export default class WholeCalc extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      displayNumber: 0
+    }
   }
 
+  setDisplayNumber( value) {
+      console.log('hello',  value)
+    if (this.state.displayNumber === 0) {
+      this.setState({displayNumber: value})
+    } else {
+        this.setState({displayNumber: "" + this.state.displayNumber + value})
+    }
+  }
   render() {
 
     return (
 
       <main role="main">
         <h1>Content here please</h1>
-        <form>
-            <div class="container">
-          <input className="numberDisplay" name="numberdisplay" id="numberDisplay" placeholder="0"/>
-<div className="buttons">        
-          <div className="leftButtons">
-            <div className="numberButtons">
-              <NumberButtons/>
-            </div>
+        <div class="container">
+          <div className="numberDisplay">
+            {this.state.displayNumber}
+          </div>
+          <div className="buttons">
+            <div className="leftButtons">
+              <div className="numberButtons">
+                <NumberButtons onClick={(value)=> this.setDisplayNumber(value)}/>
+              </div>
 
-            <div className="formOperators">
-<FormOperators />
+              <div className="formOperators">
+                <FormOperators/>
+              </div>
+            </div>
+            <div className="mathOperators">
+              <MathOperators/>
             </div>
           </div>
-          <div className="mathOperators">
-          <MathOperators />
-          </div>
-</div>
 
-</div>
-        </form>
+        </div>
 
       </main>
     );
